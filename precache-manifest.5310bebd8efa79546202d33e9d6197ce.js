b5db653d1b6f4cd2612c5377496ad1b5204341f6
Nguyen Minh Trung <tuxdomatna@gmail.com>
2018-12-01 22:33:55 +0700

    crypto

---
+++ precache-manifest.5310bebd8efa79546202d33e9d6197ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee3b59c1979b08aa9160",
+    "url": "/static/css/main.5267a015.chunk.css"
+  },
+  {
+    "revision": "ee3b59c1979b08aa9160",
+    "url": "/static/js/main.ee3b59c1.chunk.js"
+  },
+  {
+    "revision": "92d2b99da329d7e46ef5",
+    "url": "/static/js/1.92d2b99d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f55513d1fe71bd8d9d016c35be62a792",
+    "url": "/index.html"
+  }
+];
